c947763d4c8012bde21cb5d65d1a1949dc487d8a
rushilsehgal01 <sehgalrushil@gmail.com>
2024-02-02 17:14:58 -0800

    Add home route

--- run.py
+++ run.py
@@ -1,20 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request, redirect
  
 # Flask constructor takes the name of 
 # current module (__name__) as argument.
-app = Flask(__name__)
+app = Flask(__name__, template_folder='app/templates')
  
 # The route() function of the Flask class is a decorator, 
 # which tells the application which URL should call 
 # the associated function.
-@app.route('/')
-# ‘/’ URL is bound with hello_world() function.
-def hello_world():
-    return 'Hello World'
- 
+@app.route("/", methods = ["POST", "GET"])
+def home():
+    if request.method == "POST" :
+        user = request.form["nm"]
+        return redirect(url_for("survey"))
+    else :
+        return render_template("home.html")
+
+@app.route("/survey")
+def survey():
+    return f"Survey here"
+
 # main driver function
 if __name__ == '__main__':
  
     # run() method of Flask class runs the application 
     # on the local development server.
-    app.run()
+    app.run(debug = True)
